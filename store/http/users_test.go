9e6c20d5d58f1e705094ef2928d10229a6a1c325
Alexey Golovnya <alexey.golovnya@gmail.com>
2019-10-08 21:12:31 +0700

    Simple tests for users/ endpoint

--- store/http/users_test.go
+++ store/http/users_test.go
@@ -13,10 +13,39 @@ import (
 func TestPostUser(t *testing.T) {
 	b := bytes.NewBufferString(`{"id":"test-01","email":"test-01@example.org"}`)
 
-	req, _ := http.NewRequest("POST", "/users", b)
-	resp := httptest.NewRecorder()
+	mux := newMux()
 
-	newMux().ServeHTTP(resp, req)
+	{
+		req, _ := http.NewRequest("POST", "/users", b)
+		resp := httptest.NewRecorder()
 
-	assert.Equal(t, http.StatusOK, resp.Code, "OK response is expected")
+		mux.ServeHTTP(resp, req)
+
+		assert.Equal(t, http.StatusCreated, resp.Code)
+	}
+
+	{
+		req, _ := http.NewRequest("GET", "/users/test-01", nil)
+		resp := httptest.NewRecorder()
+
+		mux.ServeHTTP(resp, req)
+		assert.Equal(t, http.StatusOK, resp.Code)
+	}
+
+	{
+		req, _ := http.NewRequest("DELETE", "/users/test-01", nil)
+		resp := httptest.NewRecorder()
+
+		mux.ServeHTTP(resp, req)
+		assert.Equal(t, http.StatusNoContent, resp.Code)
+	}
+
+
+	{
+		req, _ := http.NewRequest("GET", "/users/test-01", nil)
+		resp := httptest.NewRecorder()
+
+		mux.ServeHTTP(resp, req)
+		assert.Equal(t, http.StatusNotFound, resp.Code)
+	}
 }
